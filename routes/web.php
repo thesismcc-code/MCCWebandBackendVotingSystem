2a4f9aa816febca21e9bd02f29f62e57272f7798
Cedrick Alegsao <cedrickcuencaalegsao@gmail.com>
2026-02-17 10:12:49 +0800

    partial election control.

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\QuickAccessController;
 use App\Http\Controllers\ManageAccountController;
 use App\Http\Controllers\FingerPrintController;
 use App\Http\Controllers\VotingLogsController;
+use App\Http\Controllers\ElectionController;
 
 Route::get('/', [AuthController::class, 'index']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -16,3 +17,4 @@ Route::get('/manage-accounts', [ManageAccountController::class, 'index'])->name(
 Route::post('/store-new-accounts', [ManageAccountController::class, 'storeNewAcction'])->name('store.new-accounts');
 Route::get('/finger-print', [FingerPrintController::class, 'index'])->name('view.finger-print');
 Route::get('/voting-logs', [VotingLogsController::class, 'index'])->name('view.voting-logs');
+Route::get('/election-control', [ElectionController::class, 'index'])->name('view.election-controll');
